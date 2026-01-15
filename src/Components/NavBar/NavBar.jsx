bca84ae26e2f942867c9094149d97e09a0a59993
selma-lahouali <lahoualiselma@gmail.com>
2026-01-15 23:09:38 +0100

    side bar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -5,22 +5,40 @@ export default function NavBar() {
   return (
     <>
       <div id="navBar">
+        {/* logo */}
         <Link to="/HomePage">
           <img id="logo" src={logo} alt="tooth logo" />
         </Link>
+        {/* navlinks */}
         <ul id="navBarList">
-          <Link to="/AboutUs">
-            <li>A propos</li>
-          </Link>
-          <Link to="/Services">
-            <li>Services</li>
-          </Link>
-          <Link to="/Treatments">
-            <li>Traitements</li>
-          </Link>
-          <Link to="/NewPatients">
-            <li>Nouveaux patients</li>
-          </Link>
+          <li>
+            <Link to="/AboutUs"> A propos </Link>
+          </li>
+          <li>
+            <Link to="/Services"> Services </Link>
+            <ul>
+              <li>Général</li>
+              <li>Cosmetique</li>
+              <li>Sourir</li>
+            </ul>
+          </li>
+          <li>
+            <Link to="/Treatments"> Traitements </Link>
+            <ul>
+              <li>Douleur</li>
+              <li>Carries</li>
+              <li>Dentes manquantes</li>
+              <li>Dente cassé ou fracturé</li>
+              <li>Dente mobile</li>
+              <li>Sensibilité dentaire</li>
+              <li>Saigenent de la gensive</li>
+              <li>Dentes sensibles</li>
+              <li>Dents tordues</li>
+            </ul>
+          </li>
+          <li>
+            <Link to="/NewPatients"> Nouveaux patients </Link>
+          </li>
         </ul>
       </div>
     </>
